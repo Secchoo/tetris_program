f0254776d7ccc12e0a99cf1062c9d6d6813d06cb
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-06-16 14:03:54 +0800

    added grid lines for the game

--- tetris.py
+++ tetris.py
@@ -5,8 +5,14 @@ class Tetris:
     def __init__(self, app):
         self.app = app
 
+    def grid(self):
+        for x in range(field_w):
+            for y in range(field_h):
+                pg.draw.rect(self.app.screen, 'black',
+                             (x * tile_size, y * tile_size, tile_size, tile_size), 1)
+
     def update(self):
         pass
 
     def draw(self):
-        pass
+        self.grid()
