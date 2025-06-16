281d227ca1fc9cc7534422b2b7db28cb68943a5b
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-06-16 16:04:46 +0800

    tested if all blocks are correctly drawn and created a falling movement for the game

--- tetromino.py
+++ tetromino.py
@@ -1,22 +1,36 @@
 from settings import *
+import random
 
 
 class Block(pg.sprite.Sprite):
     def __init__(self, tetromino, pos):
         self.tetromino = tetromino
+        self.pos = vec(pos) + init_pos_offset
 
         super().__init__(self.tetromino.tetris.sprite_group)
         self.image = pg.Surface((tile_size, tile_size))
         self.image.fill('green')
 
         self.rect = self.image.get_rect()
-        self.rect.topleft = pos[0] * tile_size, pos[1] * tile_size
+    
+    def set_rect_pos(self):
+        self.rect.topleft = self.pos * tile_size
+
+    def update(self):
+        self.set_rect_pos()
 
 
 class Tetromino:
     def __init__(self, tetris):
         self.tetris = tetris
-        Block(self, (4, 7))
+        self.shape = random.choice(list(tetrominoes.keys()))
+        self.blocks = [Block(self, pos) for pos in tetrominoes[self.shape]]
+
+    def move(self, direction):
+        move_direction = move_directions[direction]
+        for block in self.blocks:
+            block.pos += move_direction
 
     def update(self):
-        pass    
+        self.move(direction='down')
+        pg.time.wait(200) #simulate a slow falling block    
